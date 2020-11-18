3f9d398373cb3dd8105042a458e92f34a24b1cc0
Viktor Vinitski <fireworkv0@gmail.com>
2020-11-18 20:15:19 +0300

    fixes of main task

--- UI/js/classes.js
+++ UI/js/classes.js
@@ -1,129 +1,163 @@
+'use strict';
 let count = 1;
 let idGenerator = () => (count++).toString()
-
 const messages = [
     {
+        id: idGenerator(),
         text: '111',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Cristiano Ronaldo'
     },
     {
+        id: idGenerator(),
         text: '222',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Leo Messi'
     },
     {
+        id: idGenerator(),
         text: '333',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Bruno Fernandes'
     },
     {
-         
+        id: idGenerator(),
         text: '444',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Harry Kane'
     },
     {
-         
+        id: idGenerator(),
         text: '555',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Dele Alli'
     },
     {
-         
+        id: idGenerator(),
         text: '666',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Wayne Rooney'
     },
     {
-         
+        id: idGenerator(),
         text: '777',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Harry Maguire'
     },
     {
-         
+        id: idGenerator(),
         text: '888',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'David De Gea'
     },
     {
-         
+        id: idGenerator(),
         text: '999',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Paul Pogba'
     },
     {
+        id: idGenerator(),
         text: '101010',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Donny van de Beek'
     },
     {
+        id: idGenerator(),
         text: '111111',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Ronaldinho'
     },
     {
+        id: idGenerator(),
         text: '121212',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Nemanja Matic'
     },
     {
+        id: idGenerator(),
         text: '131313',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Luke Shaw'
     },
     {
+        id: idGenerator(),
         text: '141414',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Anthony Martial'
     },
     {
+        id: idGenerator(),
         text: '151515',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Dan James'
     },
     {
+        id: idGenerator(),
         text: '161616',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Mason Greenwood'
     },
     {
+        id: idGenerator(),
         text: '171717',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Edinson Cavani'
     },
     {
+        id: idGenerator(),
         text: '181818',
+        createdAt: new Date(),
         author: 'Viktor Vinitski',
         isPersonal: true,
         to: 'Odion Ighalo'
     },
     {
+        id: idGenerator(),
         text: '191919',
+        createdAt: new Date(),
         author: 'Viktor Vinitskiy',
         isPersonal: true,
         to: 'Zlatan Ibragimovich'
     },
     {
+        id: idGenerator(),
         text: '202020',
+        createdAt: new Date(),
         author: 'Vik Vinitski',
         isPersonal: true,
         to: 'Marcus Rashford'
@@ -131,10 +165,10 @@ const messages = [
 ]
 
 class Message {
-    constructor({text, to, isPersonal, author}) {
-        this._id = idGenerator(),
+    constructor({ id, text, createdAt, to, isPersonal, author }) {
+        this._id = id,
         this.text = text,
-        this._createdAt = new Date(),
+        this._createdAt = createdAt,
         this._author = author,
         this.isPersonal = isPersonal,
         this.to = to
@@ -155,15 +189,16 @@ function isNonEmptyString(str) {
     return str && typeof str === 'string';
 }
 class MessageModel {
-    constructor(msgs = []){
+    constructor(msgs = []) {
         this._messages = msgs;
+        this._user = 'Viktor Vinitski'
     }
     getPage(
         skip = 0,
         top = 10,
         { author, dateFrom, dateTo, text } = {}
     ) {
-        let result = [...this._messages];
+        let result = this._messages.filter(item => item.author === this._user)
         if (author) {
             result = result.filter(item => item.author.toLowerCase().includes(author.toLowerCase()));
         }
@@ -184,64 +219,74 @@ class MessageModel {
         return this._messages.find(item => item.id === id);
     }
     add(msg) {
-        if(!MessageModel.validate(msg)){
-            return false;
-        }
-        this._messages.push(new Message(msg));
-        return true;
+        if (MessageModel.validate(msg)) {
+            msg.id = idGenerator();
+            msg.createdAt = new Date();
+            msg.author = this._user;
+            this._messages.push(new Message(msg));
+            return true;
+        }return false
     }
     edit(id, msg) {
         let index = this._messages.findIndex(item => item.id === id);
         if (index === -1 || !MessageModel.validateFields(msg)) {
             return false;
         }
-        Object.assign(this._messages[index], msg);
-        return true;
+        if (this._messages[index].author === this._user) {
+            Object.assign(this._messages[index], msg);
+            return true;
+        }
+        return false;
+
     }
     remove(id) {
         let index = this._messages.findIndex(item => item.id === id)
         if (index === -1) {
             return false;
         }
-        this._messages.splice(index, 1)
-        return true;
+        if (this._messages[index].author === this._user) {
+            this._messages.splice(index, 1)
+            return true;
+        }
+        return false;
     }
-    static validate({ text, author }) {
-        return isNonEmptyString(text)
-            && isNonEmptyString(author)
-            && text.length <= 200
+    static validate({ text }) {
+        return  isNonEmptyString(text) && text.length <= 200 
     }
     static validateFields({ text }) {
         return !text || text.length <= 200;
     }
-    addAll(msgs = []){
+    addAll(msgs = []) {
         let notValidMessages = [];
-        
+
         msgs.forEach(item => {
-            if(MessageModel.validate(item)){
-                this._messages.push(new Message(item));
-            } else {
-                notValidMessages.push(item);
+            let added = this.add(item);
+            if(!added){
+                notValidMessages.push(item)
             }
         })
 
         return notValidMessages;
     }
-    clear(){
+    clear() {
         return this._messages = [];
-    }   
+    }
 }
 
 let messageList = new MessageModel(messages.map(item => new Message(item)))
 
 
-console.log('addAll',messageList.addAll(msgs = [...messages])); // Добавить все сообщения из массива сообщений, возвращает невалидные
+
+
+console.log('addAll', messageList.addAll([...messages])); // Добавить все сообщения из массива сообщений, возвращает невалидные
 console.log('get', messageList.get('13')); // Найти сообщение по ID
-console.log('getPage', messageList.getPage(0, 20, { author: 'Vik' })); // Получить определенное количество сообщений
+console.log('getPage', messageList.getPage(0, 20, {author: 'Viktor'})); // Получить определенное количество сообщений
 console.log('add', messageList.add({
     text: 'asdasdasdasdasdasd',
-    author: 'Viktor Vinitski'
+    isPersonal: true,
+    to: 'Bruno'
 })); // Добавить сообщение
+console.log(messageList.getPage(0,45));
 console.log('remove', messageList.remove('15')); // Удалить сообщение
 console.log('edit', messageList.edit('16', { text: 'asdasdasd' })); // Изменить сообщение
 console.log('getPage', messageList.getPage(0, 20)); // Показать измененные сообщения
